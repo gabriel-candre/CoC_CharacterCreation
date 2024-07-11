4f63c67fd916d53c3190cb5b6dbe4e202e6e4c8c
gabriel-candre <correia.gabrielandre@gmail.com>
2024-07-11 15:41:40 -0300

    Update Strength rolling logic

--- src/src/application/Program.java
+++ src/src/application/Program.java
@@ -8,6 +8,8 @@ import entities.Character;
 public class Program {
     public static void main(String[] args) {
 
+        //Page 31 starts investigator creation on CoC 7th Ed. Digital book.\\
+
         Locale.setDefault(Locale.US);
 
         Scanner sc = new Scanner(System.in);
@@ -20,30 +22,57 @@ public class Program {
         System.out.print("Do you need the program to roll for you? (y/n) ");
         char rollOpt = sc.next().charAt(0);
 
-        while(rollOpt != 'y' && rollOpt != 'Y' && rollOpt != 'n' && rollOpt != 'N') {
-            System.out.println("Option Invalid! Try again!");
-            System.out.print("Do you need the program to roll for you? (y/n) ");
-            rollOpt = sc.next().charAt(0);
-        }
+//        while(rollOpt != 'y' && rollOpt != 'Y' && rollOpt != 'n' && rollOpt != 'N') {
+//            System.out.println("Option invalid! Try again! ");
+//            System.out.print("Do you need the program to roll for you? (y/n) ");
+//            rollOpt = sc.next().charAt(0);
+//        }
 
-        switch(rollOpt) {
-            case 'y':
-            case 'Y':
-                int strength = 0;
-                for (int i = 1; i <= 5; i++) {
-                    int roll = (int) (Math.random() * (7 - 1) + 1);
-                    System.out.println("Roll " + i + ": " + roll);
-                    strength += roll;
-                }
-                strength *= 5;
 
-                System.out.println("Your Strength: " + strength);
+        char reRoll = 'y';
+        while (reRoll == 'y' || reRoll == 'Y') {
+            int strength = 0;
+            switch(rollOpt) {
+                case 'y':
+                case 'Y':
 
-                character.setStrength(strength);
+                    for (int i = 1; i <= 3; i++) {
+                        int roll = (int) (Math.random() * (7 - 1) + 1);
+                        System.out.println("Roll " + i + ": " + roll);
+                        strength += roll;
+                    }
+                    strength *= 5;
 
-                break;
-            default:
-                break;
+                    System.out.println("Your Strength: " + strength);
+
+                    break;
+                case 'n':
+                case 'N':
+                    int playerRoll;
+                    for (int i = 1; i <= 3; i++) {
+                        System.out.print("Enter roll " + i + ": ");
+                        playerRoll = sc.nextInt();
+                        strength += playerRoll;
+                    }
+                    strength *= 5;
+
+                    System.out.println("Your Strength: " + strength);
+
+                    break;
+                default:
+                    System.out.println("Option Invalid! Try again!");
+                    System.out.print("Do you need the program to roll for you? (y/n) ");
+                    rollOpt = sc.next().charAt(0);
+                    break;
+            }
+            System.out.print("Do you want to reroll? (y/n) ");
+            reRoll = sc.next().charAt(0);
+            if (reRoll == 'y' || reRoll == 'Y') {
+                System.out.print("Do you need the program to roll for you? (y/n) ");
+                rollOpt = sc.next().charAt(0);
+            } else {
+             rollOpt = 'n';
+            }
         }
 
         sc.close();
